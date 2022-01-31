0fd22110894aefa183a27b69d77cfc49e220a5da
dillon-1999 <60618033+dillon-1999@users.noreply.github.com>
2022-01-31 11:47:57 -0600

    re-organized middle ware, ignored node modules

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -5,4 +5,9 @@ const usersRouter = express.Router();
 const argon2 = require("argon2");
 const { userModel } = require("../Models/UserModel");
 
+usersRouter.get("/", async (req, res) =>{
+    res.send("users page!");
+});
+
+
 module.exports = usersRouter;
--- index.js
+++ index.js
@@ -2,22 +2,14 @@
 
 // allows the use of .env file
 require("dotenv").config();
-
 // will use later, variable depending on mode
 const isProduction = process.env.MODE === "production";
-
 // create and instantiate express (our backend framework)
 const express = require("express");
 const app = express();
-
 // set the view engine, we will be using ejs
-app.set('view engine', 'ejs');
-
 // allows the parsing of json
-app.use(express.json());
 // allows parsing of url encoded bodies
-app.use(express.urlencoded({ extended: false }));
-
 // redis is our session store
 const redis = require('redis');
 const session = require('express-session');
@@ -31,17 +23,20 @@ const sessionConfig = {
     saveUninitialized: false,
     name: "session",
     cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 8 // 8 hours
+        httpOnly: true,
+        maxAge: 1000 * 60 * 8 // 8 hours
     }
 };
 
-const usersRouter = require("./Controllers/UserController");
-const friendsRouter = require("./Controllers/FriendController");
-const projectsRouter = require("./Controllers/ProjectController");
-const charactersRouter = require("./Controllers/CharacterController");
-const favoritesRouter = require("./Controllers/FavoriteController");
-const starsInRouter = require("./Controllers/StarsInController");
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const usersRouter = require("./Controllers/UserController.js");
+const friendsRouter = require("./Controllers/FriendController.js");
+const projectsRouter = require("./Controllers/ProjectController.js");
+const charactersRouter = require("./Controllers/CharacterController.js");
+const favoritesRouter = require("./Controllers/FavoriteController.js");
+const starsInRouter = require("./Controllers/StarsInController.js");
 
 app.use(session(sessionConfig));
 app.use('/users', usersRouter);
